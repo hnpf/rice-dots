77df69ce590c8eaaa40ef713227f7ddb06360625
99screams_per_minute <stevenhighsmith0@gmail.com>
2025-08-03 18:03:00 -0400

    Delete dotfiles/breeze-rose-pine-dawn directory

--- dotfiles/breeze-rose-pine-dawn/images/spinner/rotate.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-open("spinner360.png", "wb+").write(open("spinner0.png", "rb").read())
-
-for deg in range(0, 90, 10):
-    img = Image.open("spinner" + str(deg) + ".png")
-    img.rotate(-90).save("spinner" + str(deg + 90) + ".png")
-    img.rotate(180).save("spinner" + str(deg + 180) + ".png")
-    img.rotate(90).save("spinner" + str(deg + 270) + ".png")
